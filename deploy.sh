17b884a746e446b2f61496c97079f7697cc38016
Poovarasan <50748540+flowerpoo@users.noreply.github.com>
2024-03-02 12:32:46 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,4 @@ sudo apt-get install -y python3 python3-pip
 git clone https://github.com/flowerpoo/CI-CD-Assignment.git
 cd CI-CD-Assignment
 sudo pip install -r requirements.txt
-python3 app.py &
-gunicorn -w 4 -b 0.0.0.0:5000 app:app
+nohup python3 app.py > flask.log 2>&1 &
