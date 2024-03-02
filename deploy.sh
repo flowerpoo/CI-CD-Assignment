95ff06b5fe3cc778a069f9e1dc0ec617b40be565
Poovarasan <50748540+flowerpoo@users.noreply.github.com>
2024-03-02 11:56:28 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ sudo apt-get install -y python3 python3-pip
 git clone https://github.com/flowerpoo/CI-CD-Assignment.git
 cd CI-CD-Assignment
 sudo pip install -r requirements.txt
-python3 app.py
+python3 app.py &
